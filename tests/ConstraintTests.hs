2e325792fb152b8c7fe0a4bc0abd24069e285bf9
Tristan Ravitch <travitch@cs.wisc.edu>
2012-09-13 14:30:23 -0500

    Test cleanups

--- tests/ConstraintTests.hs
+++ tests/ConstraintTests.hs
@@ -1,6 +1,6 @@
 module Main ( main ) where
 
-import Data.List ( sort )
+import Data.List ( permutations, sort )
 import Test.Framework ( defaultMain, testGroup, Test )
 import Test.Framework.Providers.HUnit
 import Test.HUnit hiding ( Test )
@@ -25,49 +25,34 @@ tests = [
   ]
 
 tc1 :: Assertion
-tc1 =
-  assertEqual "tc1" [5,6 :: Int] (sort sol)
+tc1 = solveFor "tc1" "a" [5,6] is
   where
-    Just solved = solveSystem cs
-    Just sol = leastSolution solved "a"
-    cs = constraintSystem [ atom 5 <=! setVariable "a", atom 6 <=! setVariable "a" ]
+    is = [ atom 5 <=! setVariable "a", atom 6 <=! setVariable "a" ]
 
 tc2 :: Assertion
-tc2 =
-  assertEqual "tc2" [5 :: Int] (sort sol)
+tc2 = solveFor "tc2" "a" [5] is
   where
-    Just solved = solveSystem cs
-    Just sol = leastSolution solved "a"
-    cs = constraintSystem [ atom 5 <=! setVariable "a", atom 6 <=! setVariable "b" ]
+    is = [ atom 5 <=! setVariable "a", atom 6 <=! setVariable "b" ]
 
 tc3 :: Assertion
-tc3 =
-  assertEqual "tc3" [5, 6 :: Int] (sort sol)
+tc3 = solveFor "tc3" "a" [5,6] is
   where
-    Just solved = solveSystem cs
-    Just sol = leastSolution solved "a"
-    cs = constraintSystem [ atom 5 <=! setVariable "b"
-                          , atom 6 <=! setVariable "b"
-                          , setVariable "b" <=! setVariable "a"
-                          ]
+    is = [ atom 5 <=! setVariable "b"
+         , atom 6 <=! setVariable "b"
+         , setVariable "b" <=! setVariable "a"
+         ]
 
 tc4 :: Assertion
-tc4 =
-  assertEqual "tc4" [0..20 :: Int] (sort sol)
+tc4 = solveFor "tc4" "a" [0..20] is
   where
-    Just solved = solveSystem cs
-    Just sol = leastSolution solved "a"
-    cs = constraintSystem $ map ((<=! setVariable "a") . atom) [0..20]
+    is = map ((<=! setVariable "a") . atom) [0..20]
 
 -- From the FFSA98 paper
 tc5 :: Assertion
-tc5 =
-  assertEqual "tc5" [0..40 :: Int] (sort sol)
+tc5 = mapM_ (solveFor "tc5" "R1" [0..40]) $ take 1000 $ permutations is
   where
-    Just solved = solveSystem cs
-    Just sol = leastSolution solved "R1"
-    cs = constraintSystem $ concat [
-      [setVariable "Z" <=! setVariable "R1"
+    is = concat [
+      [ setVariable "Z" <=! setVariable "R1"
       , setVariable "Z" <=! setVariable "R2"
       , setVariable "Y1" <=! setVariable "Z"
       , setVariable "Y2" <=! setVariable "Z"
@@ -82,116 +67,107 @@ tc5 =
 
 -- Test a simple cycle
 tc6 :: Assertion
-tc6 =
-  assertEqual "tc6" [5, 6, 7, 8 :: Int] (sort sol)
+tc6 = mapM_ (solveFor "tc6" "a" [5,6,7,8]) $ take 1000 $ permutations is
   where
-    Just solved = solveSystem cs
-    Just sol = leastSolution solved "a"
-    cs = constraintSystem [ atom 5 <=! setVariable "b"
-                          , atom 6 <=! setVariable "b"
-                          , atom 7 <=! setVariable "a"
-                          , atom 8 <=! setVariable "a"
-                          , setVariable "b" <=! setVariable "a"
-                          , setVariable "a" <=! setVariable "b"
-                          ]
+    is = [ atom 5 <=! setVariable "b"
+         , atom 6 <=! setVariable "b"
+         , atom 7 <=! setVariable "a"
+         , atom 8 <=! setVariable "a"
+         , setVariable "b" <=! setVariable "a"
+         , setVariable "a" <=! setVariable "b"
+         ]
 
 -- Make a longer cycle
 tc7 :: Assertion
-tc7 =
-  assertEqual "tc7" [5, 6, 7, 8 :: Int] (sort sol)
+tc7 = mapM_ (solveFor "tc7" "f" [5,6,7,8]) $ take 1000 $ permutations is
   where
-    Just solved = solveSystem cs
-    Just sol = leastSolution solved "f"
-    cs = constraintSystem [ atom 5 <=! setVariable "b"
-                          , atom 6 <=! setVariable "b"
-                          , atom 7 <=! setVariable "a"
-                          , atom 8 <=! setVariable "a"
-                          , setVariable "b" <=! setVariable "a"
-                          , setVariable "c" <=! setVariable "b"
-                          , setVariable "d" <=! setVariable "c"
-                          , setVariable "e" <=! setVariable "d"
-                          , setVariable "f" <=! setVariable "e"
-                          , setVariable "g" <=! setVariable "f"
-                          , setVariable "a" <=! setVariable "g"
-                          ]
+    is = [ atom 5 <=! setVariable "b"
+         , atom 6 <=! setVariable "b"
+         , atom 7 <=! setVariable "a"
+         , atom 8 <=! setVariable "a"
+         , setVariable "b" <=! setVariable "a"
+         , setVariable "c" <=! setVariable "b"
+         , setVariable "d" <=! setVariable "c"
+         , setVariable "e" <=! setVariable "d"
+         , setVariable "f" <=! setVariable "e"
+         , setVariable "g" <=! setVariable "f"
+         , setVariable "a" <=! setVariable "g"
+         ]
 
 
 tc8 :: Assertion
-tc8 =
-  assertEqual "tc8" ([] :: [Int]) (sort sol)
+tc8 = mapM_ (solveFor "tc8" "zz" []) $ take 1000 $ permutations is
   where
-    Just solved = solveSystem cs
-    Just sol = leastSolution solved "zz"
-    cs = constraintSystem [ atom 5 <=! setVariable "b"
-                          , atom 6 <=! setVariable "b"
-                          , atom 7 <=! setVariable "a"
-                          , atom 8 <=! setVariable "a"
-                          , setVariable "b" <=! setVariable "a"
-                          , setVariable "c" <=! setVariable "b"
-                          , setVariable "d" <=! setVariable "c"
-                          , setVariable "e" <=! setVariable "d"
-                          , setVariable "f" <=! setVariable "e"
-                          , setVariable "g" <=! setVariable "f"
-                          , setVariable "a" <=! setVariable "g"
-                          , setVariable "z" <=! setVariable "f"
-                          , setVariable "zz" <=! setVariable "z"
-                          ]
+    is = [ atom 5 <=! setVariable "b"
+         , atom 6 <=! setVariable "b"
+         , atom 7 <=! setVariable "a"
+         , atom 8 <=! setVariable "a"
+         , setVariable "b" <=! setVariable "a"
+         , setVariable "c" <=! setVariable "b"
+         , setVariable "d" <=! setVariable "c"
+         , setVariable "e" <=! setVariable "d"
+         , setVariable "f" <=! setVariable "e"
+         , setVariable "g" <=! setVariable "f"
+         , setVariable "a" <=! setVariable "g"
+         , setVariable "z" <=! setVariable "f"
+         , setVariable "zz" <=! setVariable "z"
+         ]
 
 tc9 :: Assertion
 tc9 =
-  assertEqual "tc9" ([11] :: [Int]) (sort sol)
+  mapM_ (solveFor "tc9" "zz" [11]) $ take 1000 $ permutations is
   where
-    Just solved = solveSystem cs
-    Just sol = leastSolution solved "zz"
-    cs = constraintSystem [ atom 5 <=! setVariable "b"
-                          , atom 6 <=! setVariable "b"
-                          , atom 7 <=! setVariable "a"
-                          , atom 8 <=! setVariable "a"
-                          , setVariable "b" <=! setVariable "a"
-                          , setVariable "c" <=! setVariable "b"
-                          , setVariable "d" <=! setVariable "c"
-                          , setVariable "e" <=! setVariable "d"
-                          , setVariable "f" <=! setVariable "e"
-                          , setVariable "g" <=! setVariable "f"
-                          , setVariable "a" <=! setVariable "g"
-                          , setVariable "z" <=! setVariable "f"
-                          , setVariable "zz" <=! setVariable "z"
-                          , atom 11 <=! setVariable "zz"
-                          ]
+    is = [ atom 5 <=! setVariable "b"
+         , atom 6 <=! setVariable "b"
+         , atom 7 <=! setVariable "a"
+         , atom 8 <=! setVariable "a"
+         , setVariable "b" <=! setVariable "a"
+         , setVariable "c" <=! setVariable "b"
+         , setVariable "d" <=! setVariable "c"
+         , setVariable "e" <=! setVariable "d"
+         , setVariable "f" <=! setVariable "e"
+         , setVariable "g" <=! setVariable "f"
+         , setVariable "a" <=! setVariable "g"
+         , setVariable "z" <=! setVariable "f"
+         , setVariable "zz" <=! setVariable "z"
+         , atom 11 <=! setVariable "zz"
+         ]
 
 tc10 :: Assertion
 tc10 =
-  assertEqual "tc10" ([5, 6, 7, 8, 11] :: [Int]) (sort sol)
+  mapM_ (solveFor "tc10" "c" [5,6,7,8,11]) $ take 1000 $ permutations is
   where
-    Just solved = solveSystem cs
-    Just sol = leastSolution solved "c"
-    cs = constraintSystem [ atom 5 <=! setVariable "b"
-                          , atom 6 <=! setVariable "b"
-                          , atom 7 <=! setVariable "a"
-                          , atom 8 <=! setVariable "a"
-                          , setVariable "b" <=! setVariable "a"
-                          , setVariable "c" <=! setVariable "b"
-                          , setVariable "d" <=! setVariable "c"
-                          , setVariable "e" <=! setVariable "d"
-                          , setVariable "f" <=! setVariable "e"
-                          , setVariable "g" <=! setVariable "f"
-                          , setVariable "a" <=! setVariable "g"
-                          , setVariable "z" <=! setVariable "f"
-                          , setVariable "zz" <=! setVariable "z"
-                          , atom 11 <=! setVariable "zz"
-                          ]
+    is = [ atom 5 <=! setVariable "b"
+         , atom 6 <=! setVariable "b"
+         , atom 7 <=! setVariable "a"
+         , atom 8 <=! setVariable "a"
+         , setVariable "b" <=! setVariable "a"
+         , setVariable "c" <=! setVariable "b"
+         , setVariable "d" <=! setVariable "c"
+         , setVariable "e" <=! setVariable "d"
+         , setVariable "f" <=! setVariable "e"
+         , setVariable "g" <=! setVariable "f"
+         , setVariable "a" <=! setVariable "g"
+         , setVariable "z" <=! setVariable "f"
+         , setVariable "zz" <=! setVariable "z"
+         , atom 11 <=! setVariable "zz"
+         ]
 
 -- There are no solutions to this type of constraint (A ⊆ c) when c is
 -- a nullary constructor (a constant term).  It can have solutions
 -- when c has arguments.
 tc11 :: Assertion
-tc11 =
-  assertEqual "tc11" ([] :: [Int]) (sort sol)
+tc11 = solveFor "tc11" "a" [] is
   where
-    Just solved = solveSystem cs
-    Just sol = leastSolution solved "a"
-    cs = constraintSystem [ setVariable "a" <=! atom 5 ]
+    is = [ setVariable "a" <=! atom 5 ]
 
+solveFor :: String -> String -> [Int] -> [Inclusion String Int] -> Assertion
+solveFor name var expected is =
+  assertEqual name (sort expected) (sort sol)
+  where
+    cs = constraintSystem is
+    Just solved = solveSystem cs
+    Just sol = leastSolution solved var
 
 main :: IO ()
 main = defaultMain tests
