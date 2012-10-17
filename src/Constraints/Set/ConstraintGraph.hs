47d6ab46cfe6afc4bc6de367f4befbc28e7f92b2
Tristan Ravitch <travitch@cs.wisc.edu>
2012-10-17 14:10:55 -0500

    Switch to IntMap in the custom graph
    In benchmarks, it seems to be faster (though it allocates a bit more
    than) a HashMap Int a

--- src/Constraints/Set/ConstraintGraph.hs
+++ src/Constraints/Set/ConstraintGraph.hs
@@ -11,13 +11,13 @@ module Constraints.Set.ConstraintGraph (
   graphEdges
   ) where
 
-import Data.HashMap.Strict ( HashMap )
-import qualified Data.HashMap.Strict as HM
+import Data.IntMap ( IntMap )
+import qualified Data.IntMap as IM
 import Data.Monoid
 
 -- Pred Succ
-data Adj e = Adj (HashMap Int e) (HashMap Int e)
-type Gr e = HashMap Int (Adj e)
+data Adj e = Adj (IntMap e) (IntMap e)
+type Gr e = IntMap (Adj e)
 
 newtype Graph e = Graph { unGraph :: Gr e }
 
@@ -29,46 +29,46 @@ emptyGraph = Graph mempty
 
 insEdge :: Int -> Int -> e -> Graph e -> Graph e
 insEdge src dst lbl (Graph g) =
-  let g1 = HM.adjust (addSucc dst lbl) src g
-      g2 = HM.adjust (addPred src lbl) dst g1
+  let g1 = IM.adjust (addSucc dst lbl) src g
+      g2 = IM.adjust (addPred src lbl) dst g1
   in Graph g2
 
 addSucc :: Int -> e -> Adj e -> Adj e
-addSucc dst lbl (Adj ps ss) = Adj ps (HM.insert dst lbl ss)
+addSucc dst lbl (Adj ps ss) = Adj ps (IM.insert dst lbl ss)
 
 addPred :: Int -> e -> Adj e -> Adj e
-addPred src lbl (Adj ps ss) = Adj (HM.insert src lbl ps) ss
+addPred src lbl (Adj ps ss) = Adj (IM.insert src lbl ps) ss
 
 insNode :: Int -> Graph e -> Graph e
-insNode nid = Graph . HM.insert nid emptyAdj . unGraph
+insNode nid = Graph . IM.insert nid emptyAdj . unGraph
 
 edgeExists :: Graph e -> Int -> Int -> Bool
 edgeExists (Graph g) src dst =
-  case HM.lookup src g of
+  case IM.lookup src g of
     Nothing -> False
-    Just (Adj _ ss) -> HM.member dst ss
+    Just (Adj _ ss) -> IM.member dst ss
 
 foldlPred :: (a -> Int -> e -> a) -> a -> Graph e -> Int -> a
 foldlPred f seed (Graph g) nid =
-  case HM.lookup nid g of
+  case IM.lookup nid g of
     Nothing -> seed
-    Just (Adj ps _) -> HM.foldlWithKey' f seed ps
+    Just (Adj ps _) -> IM.foldlWithKey' f seed ps
 
 foldlSucc :: (a -> Int -> e -> a) -> a -> Graph e -> Int -> a
 foldlSucc f seed (Graph g) nid =
-  case HM.lookup nid g of
+  case IM.lookup nid g of
     Nothing -> seed
-    Just (Adj _ ss) -> HM.foldlWithKey' f seed ss
+    Just (Adj _ ss) -> IM.foldlWithKey' f seed ss
 
 nodes :: Graph e -> [Int]
-nodes = HM.keys . unGraph
+nodes = IM.keys . unGraph
 
 graphNodes :: Graph e -> [(Int, ())]
-graphNodes (Graph g) = zip (HM.keys g) (repeat ())
+graphNodes (Graph g) = zip (IM.keys g) (repeat ())
 
 graphEdges :: Graph e -> [(Int, Int, e)]
-graphEdges = HM.foldrWithKey edgesForNode [] . unGraph
+graphEdges = IM.foldrWithKey edgesForNode [] . unGraph
   where
     edgesForNode nid (Adj _ ss) acc =
-      HM.foldrWithKey (mkEdge nid) acc ss
+      IM.foldrWithKey (mkEdge nid) acc ss
     mkEdge srcid dstid lbl acc = (srcid, dstid, lbl) : acc
